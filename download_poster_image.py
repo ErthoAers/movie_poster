6adb3c2865df2ee4894f0e528d175039277f3b0f
icefiresS <me@icefires.cc>
2018-05-13 19:33:19 +0800

    Create the multithreads downloader

--- download_poster_image.py
+++ download_poster_image.py
@@ -1,30 +1,37 @@
 import json, requests, re, sys
+from multiprocessing.dummy import Pool as ThreadPool
 
 def get_movie_id_from_url(url):
         if url is None:
             return None
         return re.search("(tt[0-9]{7})", url).group()
 
+i = 0
+def download_poster(m):
+    global i
+    if m['image_urls'] != []:
+        movie_num = get_movie_id_from_url(m['movie_imdb_link'])
+        with open('poster/%s.jpg' % movie_num, 'wb') as p:
+            r = requests.get(m['image_urls'][0])
+            p.write(r.content)
+        i += 1
+        if i % 1000 == 0:
+            sys.stdout.write("\r%d" % i + ' complete')
+            sys.stdout.flush()
+        return {movie_num:m}
+    return None
+
 def downloader():
     print("********** Download posters **********")
     print("Loading the JSON file...")
     with open('imdb_output.json') as f:
         movies = json.load(f)
-    movies_processed, i = [], 0
     print("Loading finish.")
 
-    for m in movies:
-        if m['image_urls'] != []:
-            movie_num = get_movie_id_from_url(m['movie_imdb_link'])
-            with open('poster/%s.jpg' % movie_num, 'wb') as p:
-                r = requests.get(m['image_urls'][0])
-                p.write(r.content)
-            movies_processed.append({movie_num:m})
-            i += 1
-            if i % 1000 == 0:
-                sys.stdout.write("\r%d" % i + ' complete')
-                sys.stdout.flush() 
-    sys.stdout.write("\rDownload complete, saved %d posters.\n\n" % (i))
+    pool = ThreadPool(16)
+    movies_first_processed = pool.map(download_poster, movies)
+    movies_processed = [m for m in movies_first_processed if m != None]
+    print("Download complete, saved %d posters." % (len(movies_processed)))
 
     print("Saving processed imdb data...")
     with open('imdb_process.json', 'w') as f:
