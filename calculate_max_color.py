20bc8f1095a187d330226b92b2200f3b9cf19b64
icefiresS <me@icefires.cc>
2018-05-28 15:07:00 +0800

    Final change for the strange bug

--- calculate_max_color.py
+++ calculate_max_color.py
@@ -45,10 +45,11 @@ for m in movies:
         pix_data = get_pix_data(film_id, new_height)
         theme = get_theme(pix_data, max_color)
         if (theme[:, 0] == theme[:, 1]) and (theme[:, 1] == theme[:, 2]):
+            os.system('rm -rf ./color_model/*')
             theme = get_theme(pix_data, max_color) 
+        os.system('rm -rf ./color_model/*')
         print([[int(j) for j in list(i)] for i in list(theme)])
         themefile.write(json.dumps({film_id:[[int(j) for j in list(i)] for i in list(theme)]}) + ',\n')
-        os.system('rm -rf ./color_model/*')
         print("Film {0}: KMeans Time cost: {1}".format(film_id, round(time.process_time() - start, 6)))
         #img_palette(pix_data, theme)
     except:
